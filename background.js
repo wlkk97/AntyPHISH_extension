8eb2bf0ee076f74adb93a9e903bfd11d718b779b
CuracaoPL <65177062+CuracaoPL@users.noreply.github.com>
2020-05-12 23:26:11 +0200

    VERSION 1.0 - dziala

--- background.js
+++ background.js
@@ -1,72 +1,58 @@
-// var link ='"' +  document.domain + '"';
-// $.ajax({
-//     headers: { 
-//         'Accept': 'application/json',
-//         'Content-Type': 'application/json' 
-//     },
-//     type: "POST",
-//     url: "http://192.168.0.87:50000/api/v1/verify/all",
-//     data: '{"url":"weka.pwr.edu.pl"}',
-//     success: handleData
-// });
-// function handleData(data){
-//     console.log(data['status']);
-// }
-// chrome.runtime.onMessage.addListener(gotMessage);
+var table = new Array();
 
-// function gotMessage(message, sender, sendReponse) {
-//     console.log(message.url);
-// }
-//chrome.tabs.query({active: true, lastFocusedWindow: true}, tabs => {
-//    var tab = tabs[0];
- //   var url = new URL(tab.url)
-  //  let domain = {
-  //      url: url.hostname
-//    }
-//    console.log(domain.url);
-//});
-
-/*
-chrome.runtime.onMessage.addListener( function(request,sender,sendResponse)
-{
-    if( request.greeting === "GetURL" )
-    {
-        var tabURL = "Not set yet";
-        chrome.tabs.query({active:true},function(tabs){
-            if(tabs.length === 0) {
-                sendResponse({});
-                return;
-            }
-            tabURL = tabs[0].url;
-            sendResponse( {navURL:tabURL} );
-        });        
-    }
-})
-*/
-
-/*chrome.runtime.onMessage.addListener(gotMessage);
-
-function gotMessage(message, sender, sendReponse) {
-    console.log(message);
-}*/
-
-/*chrome.runtime.onMessage.addListener(
-    function(request, sender, sendResponse) {
-      if (request.cmd == "any command") {
-        sendResponse({ result: "any response from background" });
-      } else {
-        sendResponse({ result: "error", message: `Invalid 'cmd'` });
-      }
-      // Note: Returning true is required here!
-      //  ref: http://stackoverflow.com/questions/20077487/chrome-extension-message-passing-response-not-sent
-      return true; 
-    });*/
-
-
-// otrzymanie od popup.js adresu url i odeslanie wiadomosci o poprawnym przyjeciu
 chrome.runtime.onMessage.addListener(
     (request, sender, sendResponse) => {
-    if (request.message === request.message )
-        sendResponse({message: "poprawnie otrzymano adres URL: " + request.message});
-        console.log(request.message)
-    }); 
+        if (request.message === request.message) {
+            // sendResponse({message: "Got it!"});
+            // console.log(request.message);
+            let domain = {
+                "url": request.message
+            }
+            $.ajax({
+                headers: {
+                    'Accept': 'application/json',
+                    'Content-Type': 'application/json'
+                },
+                type: "POST",
+                url: "https://phish.arqsz.net/api/v1/verify/all",
+                data: JSON.stringify(domain),
+                success: handleData
+            });
+
+            function handleData(data) {
+                // console.log(data.result);
+                table[request.message] = data.result;
+                // console.log(table);
+                chrome.runtime.sendMessage({
+                    message: data.result
+                }, (response) => {
+                    console.log(response.message);
+                });
+            }
+        }
+    });
+
+chrome.runtime.onConnect.addListener(function (port) {
+    console.assert(port.name == "knockknock");
+    port.onMessage.addListener(function (msg) {
+        if (msg.joke == "Knock knock")
+            port.postMessage({
+                question: "Who's there?"
+            });
+        else if (msg.domain === msg.domain)
+            port.postMessage({
+                status: table[msg.domain]
+            });
+        else if (msg.answer == "Madame... Bovary")
+            port.postMessage({
+                question: "I don't get it."
+            });
+    });
+});
+// chrome.runtime.onMessage.addListener(function(message, sender, sendResponse) {
+//     if (message.request === message.request) {
+//         console.log(message.request)
+//         sendResponse({message: table[message.request]});
+//         console.log('wyslano: ' + table[message.request])
+//     }
+// });
--- popup.js
+++ popup.js
@@ -1,36 +1,64 @@
-// chrome.runtime.onMessage.addListener(messageReceived);
-// function messageReceived(msg) {
-//     document.getElementsByClassName("domain_result")[0].innerHTML=(msg);
-// }
-// document.getElementsByClassName("domain_result")[0].innerHTML= 'test';
-
-// var port = chrome.extension.connect({
-//     name: "Sample Communication"
-// });
-// port.postMessage("Hi BackGround");
-// port.onMessage.addListener(function(msg) {
-//     console.log("message recieved" + msg);
-// });
-
-
-
-
-chrome.tabs.query({active: true, lastFocusedWindow: true}, tabs => { 
-    var tab = tabs[0];
-    var url = new URL(tab.url)
-    let domain = {
-        url: url.hostname
-    }
-    document.getElementsByClassName("domain_hostname")[0].innerHTML= domain.url;  //wysyla do pliku html ktory wyswietla
-    chrome.tabs.sendMessage(tab.id, domain.url);  //wysyla domene do script.js na klikniecie wtyczki. tab.url zamiast domaiin.url tez dziala
-    
-    /*
-    //wysylanie do background.js adresu url
-    chrome.runtime.sendMessage({message: domain.url}, (response) => {
-        console.log(response.message);
-    });
-    */
-});
-
-
-
+  chrome.tabs.query({
+      active: true,
+      lastFocusedWindow: true
+  }, tabs => {
+      var tab = tabs[0];
+      var url = new URL(tab.url)
+      let domain = {
+          url: url.hostname
+      }
+      document.getElementsByClassName("domain_hostname")[0].innerHTML = domain.url; //wysyla do pliku html ktory wyswietla
+      //   chrome.tabs.sendMessage(tab.id, domain.url); //wysyla domene do script.js na klikniecie wtyczki. tab.url zamiast domaiin.url tez dziala
+      var port = chrome.runtime.connect({
+          name: "knockknock"
+      });
+      port.postMessage({
+          joke: "Knock knock"
+      });
+      port.onMessage.addListener(function (msg) {
+          if (msg.question == "Who's there?")
+              port.postMessage({
+                  domain: domain.url
+              });
+          else if (msg.status == 'suspicious') {
+              console.log('kurwa dziala');
+              document.getElementById('result').style.color = 'yellow';
+              document.getElementById('result').innerHTML = msg.status;
+          } else if (msg.status == 'good') {
+              console.log('kurwa dziala');
+              document.getElementById('result').style.color = 'green';
+              document.getElementById('result').innerHTML = msg.status;
+          } else if (msg.status == 'malicious') {
+              console.log('kurwa dziala');
+              document.getElementById('result').style.color = 'red';
+              document.getElementById('result').innerHTML = msg.status;
+          }
+      });
+      //     chrome.runtime.sendMessage(laserExtensionId, {request: domain.url}, function(response) {
+      //         console.log("prosze o var" + domain.url)
+      //         console.log('siemanko ' + response.message)
+      // });
+      /*
+      //wysylanie do background.js adresu url
+      chrome.runtime.sendMessage({message: domain.url}, (response) => {
+          console.log(response.message);
+      });
+      */
+  });
+  chrome.runtime.onMessage.addListener(
+      (request, sender, sendResponse) => {
+          if (request.message === request.message) {
+              document.getElementById('result').style.color = 'yellow';
+              document.getElementById('result').innerHTML = request.message;
+          }
+          //   sendResponse({
+          //       message: "Send!"
+          //   });
+          //   console.log(request.message);
+      });
+  window.onload = function () {
+      document.getElementsByClassName('btn_scan')[0].onclick = function () {
+          document.getElementsByClassName('gif')[0].style.display = 'block';
+          document.getElementsByClassName('btn_scan')[0].style.display = 'none';
+      }
+  }
